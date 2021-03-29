18ac31c177c1af74915054e32605e219042ea99a
Maruku <marcp265@gmail.com>
2021-03-28 22:21:10 -0400

    fixed footer

--- src/PiedDePage.jsx
+++ src/PiedDePage.jsx
@@ -4,8 +4,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Breadcrumbs from '@material-ui/core/Breadcrumbs';
 import Link from '@material-ui/core/Link';
-import HomeIcon from '@material-ui/icons/Home';
-import WhatshotIcon from '@material-ui/icons/Whatshot';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 
 const useStyles = makeStyles((theme) => ({
