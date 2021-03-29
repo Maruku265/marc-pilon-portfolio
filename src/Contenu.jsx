5ea684b5da630e744fb47e228796085c1c3b9a58
Maruku <marcp265@gmail.com>
2021-03-28 22:21:48 -0400

    fixed slider

--- src/Contenu.jsx
+++ src/Contenu.jsx
@@ -1,16 +1,8 @@
 import './Contenu.scss';
 import {SRLWrapper} from "simple-react-lightbox";
 import Slider from '@material-ui/core/Slider';
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles({
-    root: {
-      width: 300,
-    },
-  });
 
 export default function Contenu() {
-    const classes = useStyles();
     
     return(
         <div className="Contenu">
